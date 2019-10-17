1eefd06f6f668c6a73bd965ef4b97d442ceb0300
Romario Gustave <romagustave10@gmail.com>
2019-10-17 21:43:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,9 +8,10 @@ emoticons_hash = Hash.new
 emoticons_hash["get_emoticons"]= Hash.new
 emoticons_hash["get_meaning"]= Hash.new
 emoticons.each do english_word.emoticon_set
-
-
-  
+emoticons_hash["get_emoticons"][emoticon_set.first]= emiticon.last
+emoticons_hash["get_meaning"][emoticon_set.last]= english_word
+end
+emoticons_hash
 end
 
 def get_japanese_emoticon
